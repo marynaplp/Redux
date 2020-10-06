4aa691c1a36ba27efc19da3affa1ef2cc844ceb5
Maryna <maryna.pylypchenko@gmail.com>
2020-10-06 17:17:15 +0300

    commit

---
+++ Lesson1/HW/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import { UserContext } from "./userMenu-context"
+
+class App extends React.Component {
+  state = {
+    userData: {
+      name: "Nikola Tesla",
+      avatar_url: "https://avatars3.githubusercontent.com/u10001",
+    },
+  };
+  
+  render() {
+    return (
+   
+        <UserContext.Provider value={this.state.userData}>
+          <Header />
+        </UserContext.Provider>
+
+        
+
+    );
+  }
+}
+export default App
