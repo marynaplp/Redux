b37ad40fc655382d4c80deae7c7ebaf6891ce6a3
Maryna <maryna.pylypchenko@gmail.com>
2020-10-12 17:03:08 +0300

    commit

--- Lesson2/task1/src/index.js
+++ Lesson2/task1/src/index.js
@@ -1,11 +1,14 @@
 import { createStore } from "redux"
-const incrementAction = {
-    type: "INCREMENT",
+const incrementActionCreator = () => {
+    return {
+        type: "INCREMENT",
+    }
 }
-
-const decrementAction = {
-    type: "DECREMENT"
-}
+const decrementActionCreator = () => {
+    return {
+        type: "DECREMENT"
+    }
+};
 const counterReducer = (state = 0, action) => {
     switch (action.type) {
         case 'INCREMENT':
@@ -17,6 +20,7 @@ const counterReducer = (state = 0, action) => {
     }
 }
 const store = createStore(counterReducer)
-store.dispatch(incrementAction)
+store.dispatch(incrementActionCreator)
+store.dispatch(decrementActionCreator)
 
 console.log(store.getState())
