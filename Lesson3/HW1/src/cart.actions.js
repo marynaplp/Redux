087b1f7563c54b6480c33a17d014386ef4656933
Maryna <maryna.pylypchenko@gmail.com>
2020-10-22 18:36:46 +0300

    commit

--- Lesson3/HW1/src/cart.actions.js
+++ Lesson3/HW1/src/cart.actions.js
@@ -1,13 +1,12 @@
-export const ADD_PRODUCT = "CART/ADD_PRODUCT";
-export const REMOVE_PRODUCT = "CART/DELETE_PRODUCT";
-
+export const ADD_PRODUCT = 'CART/ADD_PRODUCT';
+export const REMOVE_PRODUCT = 'CART/REMOVE_PRODUCT';
 
 export const addProduct = (productData) => {
     return {
         type: ADD_PRODUCT,
         payload: {
             productData,
-        }
+        },
     };
 };
 
@@ -15,7 +14,7 @@ export const removeProduct = (productId) => {
     return {
         type: REMOVE_PRODUCT,
         payload: {
-            productId
-        }
+            productId,
+        },
     };
-};
+}
