c901c0ab93446ed235ebeeb06168e88ef31bbe60
Maryna <maryna.pylypchenko@gmail.com>
2020-10-27 23:29:07 +0200

    commit

--- Lesson5/task2/src/store.js
+++ Lesson5/task2/src/store.js
@@ -1,12 +1,13 @@
 import { createStore, combineReducers } from "redux";
-import { counterReducer } from "./counter/counter.reducer";
-import { usersReducer } from "./users/users.reducer";
+import optionsReducer from "./options/options.reducer";
 
-const appReducer = combineReducers({
-    users: usersReducer,
-    counter: counterReducer,
+const reducer = combineReducers({
+    options: optionsReducer,
 });
 
-const store = createStore(appReducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
