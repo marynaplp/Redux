37794cafa23e3a165f5334ab7d823d30c67d6826
Maryna <maryna.pylypchenko@gmail.com>
2020-11-02 23:07:41 +0200

    commit

--- Lesson6/HW1/src/weather/Weather.jsx
+++ Lesson6/HW1/src/weather/Weather.jsx
@@ -1,29 +1,34 @@
-import React, {  useEffect } from "react";
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { weatherDataSelector } from "./weather.selectors";
-import * as weatherDataReceived  from './weather.actions';
+import * as weatherActions from "./weather.actions";
 
 const Weather = ({ weatherData, getWeatherData }) => {
-   
-    console.log(weatherData);
 
     useEffect(() => {
         return getWeatherData();
     }, []);
-    
+
     return (
         <main className="weather">
             <h1 className="weather__title">Weather data</h1>
             <ul className="cities-list">
-                {weatherData.map((city) => (
-                    <li key={city.id} className="city">
-                        <span className="city__name">{city.name}</span>
-                        <span className="city__temperature">{city.temperature}</span>
+                {weatherData.map((cityObj) => (
+                    <li key={cityObj.id} className="city">
+                        <span className="city__name">{cityObj.name}</span>
+                        <span className="city__temperature">
+                            {`${cityObj.temperature} F`}
+                        </span>
                     </li>
                 ))}
             </ul>
         </main>
     );
+};
+
+Weather.propTypes = {
+    getWeatherData: PropTypes.func.isRequired,
 };
 
 const mapState = (state) => {
@@ -33,7 +38,7 @@ const mapState = (state) => {
 };
 
 const mapDispatch = {
-    getWeatherData: weatherDataReceived.getWeatherData,
+    getWeatherData: weatherActions.getWeatherData,
 };
 
 export default connect(mapState, mapDispatch)(Weather);
