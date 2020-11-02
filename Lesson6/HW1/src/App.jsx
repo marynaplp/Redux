9377fba6c5a182cca0e7f355afbefba2228ca446
Maryna <maryna.pylypchenko@gmail.com>
2020-11-02 22:32:57 +0200

    commit

--- Lesson6/HW1/src/App.jsx
+++ Lesson6/HW1/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
-import Weather from "./weather/Weather";
 import store from "./store";
+import Weather from "./weather/Weather";
 
 const App = () => {
     return (
