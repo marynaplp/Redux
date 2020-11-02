baee933a4d728e780bb4305bfdbc892e87073a6a
Maryna <maryna.pylypchenko@gmail.com>
2020-11-02 21:49:18 +0200

    commit

--- Lesson6/HW1/src/weather/weather.actions.js
+++ Lesson6/HW1/src/weather/weather.actions.js
@@ -1,8 +1,4 @@
-import { fetchWeatherData } from "./weather.gateway";
-
 export const WEATHER_DATA_RECEIVED = "WEATHER/WEATHER_DATA_RECEIVED";
-
-
 
 export const weatherDataReceived = (weatherData) => {
     return {
@@ -12,17 +8,17 @@ export const weatherDataReceived = (weatherData) => {
         },
     };
 };
-const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/cities';
+
+const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/cities";
 
 export const getWeatherData = () => {
     return function(dispatch) {
         fetch(baseUrl)
             .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error('error')
+                if (response.ok) return response.json();
 
-            }).then((weatherData) => dispatch((weatherDataReceived)));
+                throw new Error("Failed to load");
+            })
+            .then((weatherData) => dispatch(weatherDataReceived(weatherData)));
     };
 };
