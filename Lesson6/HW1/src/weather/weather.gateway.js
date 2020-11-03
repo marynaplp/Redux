e0c4b81a507f89df444361fa40f3a06825c9b0a2
Maryna <maryna.pylypchenko@gmail.com>
2020-11-03 09:36:49 +0200

    co

--- Lesson6/HW1/src/weather/weather.gateway.js
+++ Lesson6/HW1/src/weather/weather.gateway.js
@@ -1,5 +1,4 @@
-const baseUrl = 'https://5e4e9b9e6272aa0014230d88.mockapi.io/api/v1/cities';
-
+const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/cities"
 
 export const fetchWeatherData = () => fetch(baseUrl).then(response => {
     if (response.ok) {
